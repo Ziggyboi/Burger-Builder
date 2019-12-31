7033810e2a9ccb4a9f98309d4e572162f2340753
Ziggyboi <dumbleshoot@gmail.com>
2019-12-31 20:41:50 +0000

    First commit of the module to save some last minute changes: Starting Backend Module

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import classes from './NavigationItem.module.css';
-import { tsPropertySignature } from '@babel/types';
+//import { tsPropertySignature } from '@babel/types';
 
 const navigationItem = (props) => (
     <li className={classes.NavigationItem}>
